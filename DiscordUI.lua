559f582f7d0c4303b71927da5a5a7c32d5c8c5e2
realuwo <85959402+realuwo@users.noreply.github.com>
2021-06-15 21:24:40 +0300

    Update DiscordUI.lua

--- DiscordUI.lua
+++ DiscordUI.lua
@@ -265,7 +265,8 @@ function DiscordLib:Window(text)
 
 	CloseBtn.MouseButton1Click:Connect(
 		function()
-			MainFrame:TweenSize(UDim2.new(0, 0, 0, 0), Enum.EasingDirection.Out, Enum.EasingStyle.Quart, .3, true)
+			--MainFrame:TweenSize(UDim2.new(0, 0, 0, 0), Enum.EasingDirection.Out, Enum.EasingStyle.Quart, .3, true)
+			script.Parent.Parent:Destroy()
 		end
 	)
 
